15685ed0fb9654d52458aabc7974ef42f5f152e9
Pandastor <135724774+Pandastor@users.noreply.github.com>
2023-06-24 12:17:44 +0200

    Update + ou -.js

--- + ou -.js
+++ + ou -.js
@@ -1,17 +1,17 @@
 var nombre = prompt("Joueur 1 : Quel est le nombre à faire deviner ?");
 var nombrep = parseInt(nombre, 10);
-var guess = prompt("Joueur 2 : Quel est le nombre à deviner alors Potter?");
+var guess = prompt("Joueur 2 : Alors, quel est le nombre?");
 var guessp = parseInt(guess,10);
 
 while (guessp !== nombrep){
   if (guessp < nombrep){
     alert("C'est plus !");
-   guessp = prompt("Guess again !");
+   guessp = prompt("Try again !");
   }
   
   if (guessp > nombrep){
     alert("C'est moins !");
-   guessp = prompt("Guess again naze !");
+   guessp = prompt("Essaie encore !");
   }
   if (guessp == nombrep){
   alert("T'as gagné !");
